e0f394fd37b6ce6ef9af7d017c4d0d1eca18da64
Zane Mingee <zmingee@gmail.com>
2020-02-11 05:34:45 -0500

    add(config): update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true";
+static const char col_gray1[]       = "#2E3440";
+static const char col_gray2[]       = "#2E3440";
+static const char col_gray3[]       = "#D8DEE9";
+static const char col_gray4[]       = "#2E3440";
+static const char col_cyan[]        = "#88C0D0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -26,9 +26,20 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /* { class, instance, title, tags mask, isfloating, monitor } */
+
+	{ "Pavucontrol", "pavucontrol", NULL, 0, 1, -1 },
+
+    { "console", NULL, NULL, 1 << 0, 0, 0 },
+
+	{ "Claws-mail", "claws-mail", NULL, 1 << 1, 0, 1 },
+	{ "Pidgin", "Pidgin", NULL, 1 << 1, 0, 1 },
+	{ "Pidgin", "Pidgin", "Buddy List", 1 << 1, 1, 1 },
+    { "khal", NULL, NULL, 1 << 1, 0, 1 },
+	{ "Slack", NULL, NULL, 1 << 1, 0, 1 },
+    { "Ripcord", "ripcord", NULL, 1 << 1, 0, 1 },
+
+	{ "Zeal", NULL, NULL, 1 << 3, 0, 1 },
 };
 
 /* layout(s) */
@@ -59,6 +70,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -70,6 +82,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1} },
+    { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -78,7 +92,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	/*{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },*/
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
