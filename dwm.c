3973016505bfee93332a096bf2fd075369054b38
Zane Mingee <zmingee@gmail.com>
2020-09-06 18:14:20 -0400

    del: remove EnterNotify handler

--- dwm.c
+++ dwm.c
@@ -253,7 +253,6 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
 	[DestroyNotify] = destroynotify,
-	[EnterNotify] = enternotify,
 	[Expose] = expose,
 	[FocusIn] = focusin,
 	[KeyPress] = keypress,
